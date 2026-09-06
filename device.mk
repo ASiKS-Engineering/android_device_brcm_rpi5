8de030f86f15892e925cfcdf996a065f2a7c8662
ASiKS-Engineering <35295944+ASiKS-Engineering@users.noreply.github.com>
2026-09-07 00:09:44 +0200

    Add CamperNavigator to PRODUCT_PACKAGES

--- device.mk
+++ device.mk
@@ -192,3 +192,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
+
+# Camper navigator
+PRODUCT_PACKAGES += CamperNavigator
