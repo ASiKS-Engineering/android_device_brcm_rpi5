aa4d6012fe482bb151f7a58e5490e297a2e753ef
ASiKS-Engineering <35295944+ASiKS-Engineering@users.noreply.github.com>
2026-09-07 09:51:33 +0200

    Updated Directory Target and added Dynamic Theme Support

--- device.mk
+++ device.mk
@@ -198,5 +198,8 @@ PRODUCT_PACKAGES += CamperNavigator
 
 # Boot animation
 PRODUCT_COPY_FILES += \
-    device/brcm/rpi5/media/bootanimation.zip:system/media/bootanimation.zip
+    device/brcm/rpi5/media/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+
+PRODUCT_COPY_FILES += \
+    device/brcm/rpi5/media/bootanimation-dark.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation-dark.zip
 
